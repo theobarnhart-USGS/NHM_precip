f5d38401f42506089fd9fad2779a07d9225a2481
Theodore Barnhart <tbarnhart@usgs.gov>
2018-04-23 10:47:05 -0600

    increased time

--- hpc_scripts/submit_run_livneh_aggregation_HPC.sh
+++ hpc_scripts/submit_run_livneh_aggregation_HPC.sh
@@ -3,11 +3,11 @@
 #SBATCH -n 1            # number of cores needed
 #SBATCH -p normal                         # the partition you want to use, for this case prod is best
 #SBATCH --account=wymtwsc        # your account
-#SBATCH --time=40:00:00           # Overestimated guess at time, the process will be cancelled at the time limit (this case 6 hours), prod allows 21 day jobs.
+#SBATCH --time=50:00:00           # Overestimated guess at time, the process will be cancelled at the time limit (this case 6 hours), prod allows 21 day jobs.
 #SBATCH --mail-type=ALL         # Send email on all events
 #SBATCH --mail-user=tbarnhart@usgs.gov
 #SBATCH  -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)  
-#SBATCH --mem=25000            #memory in MB 
+#SBATCH --mem=35000            #memory in MB 
 
 echo Starting: $1
 
