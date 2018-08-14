3f8d215605563f87eb55938482dd347c3ff19810
Theodore Barnhart <tbarnhart@usgs.gov>
2018-08-14 10:52:09 -0600

    first crack at downloading NLDASv2

---
+++ get_NLDASv2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script to move files and then run the aggregation script
+# Theodore Barnhart | tbarnhart@usgs.gov
+
+#SBATCH --job-name=get_NLDASv2 # name that you chose
+#SBATCH -n 1            # number of cores needed
+#SBATCH -p normal                         # the partition you want to use, for this case prod is best
+#SBATCH --account=wymtwsc        # your account
+#SBATCH --time=144:00:00           # Overestimated guess at time, the process will be cancelled at the time limit (this case 6 hours), prod allows 21 day jobs.
+#SBATCH --mail-type=ALL         # Send email on all events
+#SBATCH --mail-user=tbarnhart@usgs.gov
+#SBATCH  -o %j.log                    # Sets output log file to %j ( will be the jobId returned by sbatch)
+
+cd ./data/NLDASv2/
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies --content-disposition -i ./subset_NLDAS_FORA0125_H_V002_20180724_154922.txt
