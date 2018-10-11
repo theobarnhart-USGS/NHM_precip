4f43efef78bba2b0a7e298007dd3763aa5aa772b
Theodore Barnhart <tbarnhart@usgs.gov>
2018-10-11 08:35:26 -0600

    updated conversion script for NLDAS

--- hpc_scripts/submit_pcl2cbh.sh
+++ hpc_scripts/submit_pcl2cbh.sh
@@ -2,7 +2,7 @@
 # Script to move files and then convert pcl to cbh for the NHM
 # Theodore Barnhart | tbarnhart@usgs.gov
 # 20180417
-#SBATCH --job-name=convert_liv # name that you chose
+#SBATCH --job-name=convNLDAS # name that you chose
 #SBATCH -n 1            # number of cores needed
 #SBATCH -p normal                         # the partition you want to use, for this case prod is best
 #SBATCH --account=wymtwsc        # your account
@@ -15,23 +15,14 @@
 reg=$1 # parse the first argument passed to the wrapper
 echo 'Starting Region '${reg}'.'
 
-# move files to the node:
-cp ~/projects/NHM_precipitation/data/livneh_*_reg_${reg}.pcl $GLOBAL_SCRATCH/ # move the forcing data to local drive
-
-cd $GLOBAL_SCRATCH
-
 # setup the working environment
-module load python/anaconda3
 source activate py36 
 
 # call the script and pass the region argument
-for var in {'Prec','Tmin','Tmax'}; do
+for var in {'P','Tmin','Tmax'}; do
 	
-	infl=./livneh_${var}_reg_${reg}.pcl
-	outfl=./livneh2016_region_${reg}_${var}.cbh
-	python -u ~/projects/NHM_precipitation/hpc_scripts/pickle2cbh.py $reg $var $infl $outfl
+	infl=~/projects/NHM_precipitation/data/livneh_${var}_reg_${reg}.pcl
+	outfl=/home/tbarnhart/projects/NHM_precipitation/data/NLDASv2_daily_cbh/NLDASv2_daily_region_${reg}_${var}.cbh
+	python -u pickle2cbh.py $reg $var $infl $outfl
 done
-# move finished files to storage
-echo 'Copying cbh files to storage.'
-cp livneh2016_region_${reg}_*.cbh /cxfs/projects/usgs/water/wymtwsc/tbarnhart/projects/NHM_precipitation/transfer/livneh2016/
 echo 'Region '${reg}' complete.'
