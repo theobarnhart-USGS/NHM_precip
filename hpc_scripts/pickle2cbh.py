9161ed03924f8b8f2ff95f9f85952f5fa4d45f11
Theodore Barnhart <tbarnhart@usgs.gov>
2018-11-01 15:41:50 -0600

    changed to report missing data

--- hpc_scripts/pickle2cbh.py
+++ hpc_scripts/pickle2cbh.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+import numpy as np
 
 reg = sys.argv[1] # region, first argument on the command line
 variable = sys.argv[2] # variable to convert
@@ -8,7 +9,11 @@ outfl = sys.argv[4] # outfile
 
 def pcl2cbh(infl,outfl,region,variable):
     '''convert a picle to a cbh file'''
+    print('Starting %s %s'%(reg,variable))
     dat = pd.read_pickle(infl)
+    
+    print('%s No Data Values Found'%np.sum(np.isnan(dat.values)))
+    
     fmt = '%.2f' # set output format to 2 decimal places
 
     if variable == 'Prec': # change the precip output to 3 decimal places
